28c19e534fbb8edd29a5879e4bb7fe83d69418b8
Nick <harmoniacodes@proton.me>
2023-04-18 13:51:37 -0400

    add home page

---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,73 @@
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* image and main header */}
+      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
+        {/* main header */}
+        <div className="z-10 mt-32 md:basis-3/5">
+          {/* headings */}
+          {/* move content up on page for medium size screen */}
+          <div className="md:-mt-20">
+            {/* set parent to relative and image to absolute position with before: properties */}
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] md:before:content-evolvetext">
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+            </div>
+            <p className="mt-8 text-sm">
+              State of the art equipment and custom fitness training. Get the
+              body you didn't think was possible. Evolve with us.
+            </p>
+          </div>
+          {/* actions */}
+          <div className="mt-8 flex items-center gap-8">
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
+        </div>
+        {/* image */}
+        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
+          <img alt="home-page-graphic" src={HomePageGraphic} />
+        </div>
+      </div>
+      {/* sponsors */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            {/* set width to 3/5 to avoid overlap of image and sponsors on short wide screens*/}
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img src={SponsorRedBull} alt="RedBull-sponsor-image" />
+              <img src={SponsorFortune} alt="Fortune-sponsor-image" />
+              <img src={SponsorForbes} alt="Forbes-sponsor-image" />
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Home;
